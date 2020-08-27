b53276b01178286b1a9ed63646cf32d167c20e80
Hasan Mubarok <hasan@alterra.id>
2020-08-27 10:58:35 +0700

    optimized brute force subset sum

--- subsetSum.py
+++ subsetSum.py
@@ -11,6 +11,10 @@ def subset_sum(arr, starting_index, sum):
             return True
         else:
             return False
+
+    # optimized
+    if arr[starting_index] > sum:
+        return False
      
     result_1 = subset_sum(arr, starting_index + 1, sum - arr[starting_index])
     print("result_1 ", result_1)
@@ -20,6 +24,13 @@ def subset_sum(arr, starting_index, sum):
      
     return result_1 or result_2      
 
-print(subset_sum([1, 2, 3], 0, 6))
-# print(subset_sum([1, 2, 7, 2, 2], 0, 7))
+# print(subset_sum([1, 2, 3], 0, 1))
+# print(subset_sum([1, 2, 3], 0, 2))
+# print(subset_sum([1, 2, 3], 0, 3))
+# print(subset_sum([1, 2, 3], 0, 4))
+# print(subset_sum([1, 2, 3], 0, 5))
+# print(subset_sum([1, 2, 3], 0, 6))
+# print(subset_sum([1, 2, 3], 0, 7))
+print(subset_sum([1, 2, 3], 0, -1))
+# print(subset_sum([1, 2, 9, 2, 20], 0, 20))
 
