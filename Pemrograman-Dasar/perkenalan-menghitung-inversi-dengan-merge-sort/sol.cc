f67d4760464be5cbf89ef5c8f167450bb60aa759
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-03-22 17:49:03 +0700

    debug function merge

--- Pemrograman-Dasar/perkenalan-menghitung-inversi-dengan-merge-sort/sol.cc
+++ Pemrograman-Dasar/perkenalan-menghitung-inversi-dengan-merge-sort/sol.cc
@@ -3,6 +3,7 @@
 using namespace std;
 
 long long merge(vector<int> &arr, int left, int mid, int right) {
+    cout << "di dalam merge, nilai left: " << left << ", mid: " << mid << ", dan right: " << right << endl;
     int n1 = mid - left + 1;
     int n2 = right - mid;
     long long inversions = 0;
@@ -21,15 +22,18 @@ long long merge(vector<int> &arr, int left, int mid, int right) {
     int i = 0, j = 0, k = left;
 
     while (i < n1 && j < n2) {
+        cout << "di dalam while, nilai i: " << i << ", j: " << j << ", dan k: " << k << endl;
         if (left_array[i] <= right_array[j]) {
             arr[k] = left_array[i];
             i++;
         } else {
+            cout << "masuk aloy, nilai i: " << i << ", j: " << j << ", dan k: " << k << ", dan nilai n1: " << n1 << ", dan nilai n1 - i: " << n1 - i << endl;
             inversions += (n1 - i);
             arr[k] = right_array[j];
             j++;
         }
         k++;
+        cout << "akhir while, nilai i: " << i << ", j: " << j << ", dan k: " << k << endl << endl;
     }
 
     while (i < n1) {
@@ -44,19 +48,25 @@ long long merge(vector<int> &arr, int left, int mid, int right) {
         k++;
     }
     
+    cout << "akhir merge, nilai left: " << left << ", mid: " << mid << ", dan right: " << right << ", dan nilai inversions: " << inversions << endl << endl;
     return inversions;
 }
 
 long long mergeSort(vector<int> &arr, int left, int right) {
+    cout << "di dalam mergeSort, nilai left: " << left << " dan nilai right: " << right << endl;
     long long inversions = 0;
     if (left < right) {
         int mid = (left + right) >> 1;
+        cout << "nilai mid: " << mid << endl;
         
         inversions += mergeSort(arr, left, mid);
+        cout << "nilai inversions setelah mergeSort(arr, left, mid): " << inversions << endl;
         
         inversions += mergeSort(arr, mid + 1, right);
+        cout << "nilai inversions setelah mergeSort(arr, mid + 1, right): " << inversions << endl;
         
         inversions += merge(arr, left, mid, right);
+        cout << "nilai inversions setelah merge(arr, left, mid, right): " << inversions << endl << endl;
     }
     return inversions;
 }
