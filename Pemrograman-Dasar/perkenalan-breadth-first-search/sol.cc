14000f7b872b7c90ec8c42b7823bf8602b591022
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-07 19:00:30 +0700

    debug perkenalan breadth first search

--- Pemrograman-Dasar/perkenalan-breadth-first-search/sol.cc
+++ Pemrograman-Dasar/perkenalan-breadth-first-search/sol.cc
@@ -17,6 +17,12 @@ int main()
         cin >> a[i];
         t[i] = i + 1;
     }
+    cout << "nilai a: ";
+    for (int x: a) cout << x << ' ';
+    cout << '\n';
+    cout << "nilai t: ";
+    for (int x: t) cout << x << ' ';
+    cout << '\n';
     cin >> k;
     queue<pair<vector<int>, int>> q;
     set<vector<int>> vis;
@@ -25,6 +31,9 @@ int main()
     while (!q.empty())
     {
         auto [v, d] = q.front();
+        cout << "nilai v: ";
+        for (int x: v) cout << x << ' ';
+        cout << ", dan nilai d: " << d << '\n';
         q.pop();
         if (v == t)
         {
@@ -35,10 +44,30 @@ int main()
         {
             vector<int> u = v;
             reverse(u.begin() + i, u.begin() + i + k);
+            cout << "nilai u: ";
+            for (int x: u) cout << x << ' ';
+            cout << '\n';
             if (!vis.count(u))
             {
                 vis.insert(u);
+                cout << "nilai vis: ";
+                for (auto x: vis)
+                {
+                    for (int y: x) cout << y << ' ';
+                    cout << '\n';
+                }
+                cout << '\n';
                 q.push({u, d + 1});
+            }
+            cout << "nilai q: ";
+            queue<pair<vector<int>, int>> q2 = q;
+            while (!q2.empty())
+            {
+                auto [v, d] = q2.front();
+                cout << "di dalam while q2, nilai v: ";
+                for (int x: v) cout << x << ' ';
+                cout << "di dalam while q2, nilai d: " << d << '\n';
+                q2.pop();
             }
         }
     }
