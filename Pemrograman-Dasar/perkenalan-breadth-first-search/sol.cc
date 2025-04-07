38487e2693872cdb6060aa393e4aa7dfafbb70ac
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-07 15:42:42 +0700

    init perkenalan breadth first search

--- Pemrograman-Dasar/perkenalan-breadth-first-search/sol.cc
+++ Pemrograman-Dasar/perkenalan-breadth-first-search/sol.cc
@@ -2,10 +2,46 @@
 
 using namespace std;
 
+using ll = long long;
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
+    int n, k;
+    cin >> n;
+    vector<int> a(n), t(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        t[i] = i + 1;
+    }
+    cin >> k;
+    queue<pair<vector<int>, int>> q;
+    set<vector<int>> vis;
+    vis.insert(a);
+    q.push({a, 0});
+    while (!q.empty())
+    {
+        auto [v, d] = q.front();
+        q.pop();
+        if (v == t)
+        {
+            cout << d << '\n';
+            return 0;
+        }
+        for (int i = 0; i <= v.size() - k; i++)
+        {
+            vector<int> u = v;
+            reverse(u.begin() + i, u.begin() + i + k);
+            if (!vis.count(u))
+            {
+                vis.insert(u);
+                q.push({u, d + 1});
+            }
+        }
+    }
+    cout << -1 << '\n';
     return 0;
 }
