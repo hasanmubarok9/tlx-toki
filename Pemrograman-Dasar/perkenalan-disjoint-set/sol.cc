815aeaddd31f774b9cd814cd87e91a9650a43888
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-15 06:50:41 +0700

    solve perkenalan disjoint set

--- Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
+++ Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
@@ -1,7 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5 + 5; 
+
+int n, q;
+int parent[N];
+
+
+int find(int x)
+{
+    if (parent[x] != x)
+    {
+        parent[x] = find(parent[x]);
+    }
+    return parent[x];
+}
+
+void join(int x, int y)
+{
+    x = find(x);
+    y = find(y);
+    parent[x] = y;
+}
+
+
 int main()
 {
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++)
+    {
+        parent[i] = i;
+    }
     
+    while (q--)
+    {
+        int x, a, b;
+        cin >> x >> a >> b;
+
+        if (x == 1)
+        {
+            join(a, b);
+        }
+        else
+        {
+            if (find(a) == find(b))
+            {
+                cout << "Y" << endl;
+            }
+            else
+            {
+                cout << "T" << endl;
+            }
+        }
+        
+    }
 }
