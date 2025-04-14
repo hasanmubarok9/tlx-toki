a6da521f98f458ee42d8478a4933468a06e7d8dc
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-14 09:47:05 +0700

    feat: comment perkenalan disjoint set

--- Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
+++ Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
@@ -3,49 +3,22 @@ using namespace std;
 
 const int N = 1e5 + 5;
 
-int n, nn, q, Time, num[N], low[N], cmp[N]; // n = number of nodes, nn = number of SCC, Time = time of DFS, num = number of node, low = lowest node, cmp = component
-vector<int> s, adj[N], nadj[N];             // adj = adjacency list, nadj = adjacency list of SCC
-bitset<N> f[N];                             // f = set of nodes in SCC
-bool vis[N];                                // vis = visited nodes
+int n, q;
+int parent[N];
 
-int tarjan(int u)
+int find(int x)
 {
-    num[u] = low[u] = ++Time;
-    s.emplace_back(u);
-    for (int v : adj[u])
+    if (parent[x] != x)
     {
-        if (!cmp[v])
-        {
-            low[u] = min(low[u], num[v] ? num[v] : tarjan(v));
-        }
+        parent[x] = find(parent[x]);
     }
-
-    if (low[u] == num[u])
-    {
-        int v;
-        nn++;
-        do
-        {
-            v = s.back();
-            s.pop_back();
-            cmp[v] = nn;
-        } while (v != u);
-    }
-    return low[u];
+    return parent[x];
 }
 
-void dfs(int u)
+void join(int x, int y)
 {
-    vis[u] = true;
-    f[u].set(u);
-    for (int v : nadj[u])
-    {
-        if (!vis[v])
-        {
-            dfs(v);
-        }
-        f[u] |= f[v];
-    }    
+    x = find(x);
+    y = find(y);
 }
 
 int main()
@@ -60,23 +33,23 @@ int main()
         if (x == 1)
         {
             // join a and b
-            adj[a].emplace_back(b);
-            adj[b].emplace_back(a);
+            // adj[a].emplace_back(b);
+            // adj[b].emplace_back(a);
 
-            // TODO: join a and b
-            tarjan(a);
+            // // TODO: join a and b
+            // tarjan(a);
         }
         else
         {
             // find if a and b are in the same SCC
-            if (cmp[a] == cmp[b])
-            {
-                cout << "Y" << endl;
-            }
-            else
-            {
-                cout << "T" << endl;
-            }
+            // if (cmp[a] == cmp[b])
+            // {
+            //     cout << "Y" << endl;
+            // }
+            // else
+            // {
+            //     cout << "T" << endl;
+            // }
         }
     }
 }
