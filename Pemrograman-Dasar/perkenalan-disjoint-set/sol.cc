ecf235d4b880e9a408c8e94461f1398350f1c47a
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-14 08:18:02 +0700

    feat: init perkenalan disjoint set

--- Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
+++ Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
@@ -6,6 +6,19 @@ int main()
     int n, q;
     cin >> n >> q;
 
-    
+    while (q--)
+    {
+        int x, a, b;
+        cin >> x >> a >> b;
+
+        if (x == 1)
+        {
+            // union
+        }
+        else
+        {
+            // find
+        }
+    }    
 
 }
