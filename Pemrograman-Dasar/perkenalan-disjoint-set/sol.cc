3aaa499fa9e77654e00d6c0fc74c4d73094b0dab
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-04-14 08:54:17 +0700

    feat: init perkenalan disjoint set

--- Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
+++ Pemrograman-Dasar/perkenalan-disjoint-set/sol.cc
@@ -1,9 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 1e5 + 5;
+
+int n, nn, q, Time, num[N], low[N], cmp[N]; // n = number of nodes, nn = number of SCC, Time = time of DFS, num = number of node, low = lowest node, cmp = component
+vector<int> s, adj[N], nadj[N];             // adj = adjacency list, nadj = adjacency list of SCC
+bitset<N> f[N];                             // f = set of nodes in SCC
+bool vis[N];                                // vis = visited nodes
+
+int tarjan(int u)
+{
+    num[u] = low[u] = ++Time;
+    s.emplace_back(u);
+    for (int v : adj[u])
+    {
+        if (!cmp[v])
+        {
+            low[u] = min(low[u], num[v] ? num[v] : tarjan(v));
+        }
+    }
+
+    if (low[u] == num[u])
+    {
+        int v;
+        nn++;
+        do
+        {
+            v = s.back();
+            s.pop_back();
+            cmp[v] = nn;
+        } while (v != u);
+    }
+    return low[u];
+}
+
+void dfs(int u)
+{
+    vis[u] = true;
+    f[u].set(u);
+    for (int v : nadj[u])
+    {
+        if (!vis[v])
+        {
+            dfs(v);
+        }
+        f[u] |= f[v];
+    }    
+}
+
 int main()
 {
-    int n, q;
     cin >> n >> q;
 
     while (q--)
@@ -13,12 +59,24 @@ int main()
 
         if (x == 1)
         {
-            // union
+            // join a and b
+            adj[a].emplace_back(b);
+            adj[b].emplace_back(a);
+
+            // TODO: join a and b
+            tarjan(a);
         }
         else
         {
-            // find
+            // find if a and b are in the same SCC
+            if (cmp[a] == cmp[b])
+            {
+                cout << "Y" << endl;
+            }
+            else
+            {
+                cout << "T" << endl;
+            }
         }
-    }    
-
+    }
 }
