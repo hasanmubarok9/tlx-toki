d5febf1c240d83c8c73e31b02de672410f57e53f
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-06-06 18:26:17 +0700

    debug perkenalan pohon rentang minimum prim kruskal

--- Pemrograman-Dasar/perkenalan-pohon-rentang-minimum-prim-kruskal/sol.cc
+++ Pemrograman-Dasar/perkenalan-pohon-rentang-minimum-prim-kruskal/sol.cc
@@ -7,38 +7,80 @@ using ll = long long;
 ll n, par[107];
 vector<pair<ll, pair<ll, ll>>> v;
 
-ll root(ll x) {
-    if (par[x] == x) return x;
+ll root(ll x)
+{
+    cout << "masuk root, nilai x: " << x << endl;
+    if (par[x] == x)
+    {
+        cout << "masuk par[x] == x, nilai x: " << x << endl;
+        return x;
+    }
+    cout << "masuk par[x] != x, nilai x: " << x << endl;
     return par[x] = root(par[x]);
 }
 
-void join(ll x, ll y) {
+void join(ll x, ll y)
+{
+    cout << "masuk join, nilai x: " << x << ", nilai y: " << y << endl;
     x = root(x);
     y = root(y);
-    if (x != y) par[x] = y;
+    cout << "setelah root, nilai x: " << x << ", nilai y: " << y << endl;
+    if (x != y)
+    {
+        cout << "masuk x != y, nilai x: " << x << ", nilai y: " << y << endl;
+        par[x] = y;
+    }
 }
 
-int main() {
+int main()
+{
     cin >> n;
 
-    for (ll i = 0; i < n; i++) {
+    for (ll i = 0; i < n; i++)
+    {
         par[i] = i;
     }
 
+    cout << "nilai par: ";
+    for (ll i = 0; i < n; i++)
+    {
+        cout << par[i] << " ";
+    }
+    cout << endl;
+
     ll sz = 1;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
             ll x;
             cin >> x;
-            if (i != j) v.emplace_back(x, make_pair(i, j));
+            cout << "nilai x: " << x << endl;
+            if (i != j)
+            {
+                cout << "masuk i != j, nilai i: " << i << ", nilai j: " << j << endl;
+                v.emplace_back(x, make_pair(i, j));
+            }
         }
+        cout << '\n';
     }
-    
+
+    cout << "nilai v:\n";
+    for (pair<ll, pair<ll, ll>> i : v)
+    {
+        cout << i.first << " " << i.second.first << " " << i.second.second << endl;
+    }
+
     ll ans = 0;
     sort(v.begin(), v.end());
-    for (pair<ll, pair<ll, ll>> i : v) {
-        if (root(i.second.first) != root(i.second.second)) {
+    for (pair<ll, pair<ll, ll>> i : v)
+    {
+        cout << "nilai i: " << i.first << ", " << i.second.first << ", " << i.second.second << endl;
+        if (root(i.second.first) != root(i.second.second))
+        {
+            cout << "masuk if, nilai i: " << i.first << ", " << i.second.first << ", " << i.second.second << endl;
             ans += i.first;
+            cout << "nilai ans: " << ans << endl;
             join(i.second.first, i.second.second);
         }
     }
