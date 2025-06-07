194a538b1a01edb5100e1dc50aa6235f8d13c7c6
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-06-07 15:18:55 +0700

    init perkenalan pohon rentang minimum prim kruskal

--- Pemrograman-Dasar/perkenalan-pohon-rentang-minimum-prim-kruskal/sol.cc
+++ Pemrograman-Dasar/perkenalan-pohon-rentang-minimum-prim-kruskal/sol.cc
@@ -2,8 +2,37 @@
 
 using namespace std;
 
+int n, par[107];
+vector<tuple<int, int, int>> g; // weight, node1, node2
+
 int main()
 {
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        par[i] = i;
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            int w;
+            cin >> w;
+
+            if (i != j)
+            {
+                g.emplace_back(w, i, j);
+            }
+        }
+    }
+
+    sort(g.begin(), g.end());
+
+    for (auto [w, u, v] : g)
+    {
+        cout << w << " " << u << " " << v << endl;
+    }
     
     return 0;
 }
