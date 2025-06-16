ce0c700bf55c9d0a3f05447f5086207ffe0bede1
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-06-16 19:23:52 +0700

    solve tebas tebang pohon

--- Pemrograman-Dasar/tebas-tebang-pohon/sol.cc
+++ Pemrograman-Dasar/tebas-tebang-pohon/sol.cc
@@ -20,47 +20,29 @@ int main()
         mx = max(mx, a[i]);
     }
 
-    cout << "nilai a: ";
-    for (int i = 1; i <= n; i++) cout << a[i] << " ";
-    cout << '\n';
-
     sort(a + 1, a + n + 1);
-
-    cout << "nilai a setelah di sort: ";
-    for (int i = 1; i <= n; i++) cout << a[i] << " ";
-    cout << '\n';
 
     ll l = 0, r = mx, note = -1;
     while (l <= r)
     {
-        cout << "nilai l: " << l << " nilai r: " << r << '\n';
         ll mid = (l + r) >> 1;
-        cout << "nilai mid: " << mid << '\n';
         ll cut = 0;
         for (int i = 1; i <= n; i++)
         {
-            cout << "nilai a[i]: " << a[i] << " nilai mid: " << mid << ", nilai a[i] - mid: " << a[i] - mid << '\n';
             if (a[i] > mid)
             {
-                cout << "masuk a[i] > mid. ketika nilai a[i]: " << a[i] << ", dan nilai mid: " << mid << ", nilai a[i] - mid: " << a[i] - mid << '\n';
                 cut += a[i] - mid;
             }
-            cout << "nilai cut: " << cut << '\n';
         }
-        cout << "nilai cut: " << cut << " nilai m: " << m << '\n';
         if (cut >= m)
         {
-            cout << "masuk kanan\n";
             note = mid;
             l = mid + 1;
         }
         else
         {
-            cout << "masuk kiri\n";
             r = mid - 1;
         }
-
-        cout << "akhir while, nilai l: " << l << " nilai r: " << r << " nilai note: " << note << '\n' << '\n';
     }
     cout << note << '\n';
     return 0;
