0af4ffef712b694ac8f8da9ac80c9969857e2555
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-06-16 18:05:43 +0700

    init tebas tebang pohon

---
+++ Pemrograman-Dasar/tebas-tebang-pohon/sol.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+ll n, m, a[100005], mx = 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        mx = max(mx, a[i]);
+    }
+
+    sort(a + 1, a + n + 1);
+    ll l = 0, r = mx, note = -1;
+    while (l <= r)
+    {
+        ll mid = (l + r) >> 1;
+        ll cut = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] > mid) cut += a[i] - mid;
+        }
+        if (cut >= m)
+        {
+            note = mid;
+            l = mid + 1;
+        }
+        else r = mid - 1;
+    }
+    cout << note << '\n';
+    return 0;
+}
