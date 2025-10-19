4e1a12eae989906b1a45bb87c01482d156ed3450
hasanmubarok9 <hasanmubarok32@gmail.com>
2025-10-19 17:39:04 +0700

    init knight force

---
+++ Pemrograman-Dasar/knight-force/sol.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1e9 + 5;
+const int dx[8] = { 2, 2, -2, -2, 1, 1, -1, -1 };
+const int dy[8] = { 1, -1, 1, -1, 2, -2, 2, -2 };
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m, k, s, qi, qj;
+        cin >> n >> m >> k >> s >> qi >> qj;
+
+        vector<vector<int>> dist(n, vector<int>(m, INF));
+        queue<pair<int, int>> q;
+        for (int i = 0; i < k; i++) {
+            int x, y;
+            cin >> x >> y;
+            x--; y--; // 0-based
+            dist[x][y] = 0;
+            q.emplace(x, y);
+        }
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+            if (dist[x][y] == s) continue; // cannot go further
+            for (int dir = 0; dir < 8; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+                if (dist[nx][ny] > dist[x][y] + 1) {
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.emplace(nx, ny);
+                }
+            }
+        }
+        if (dist[qi - 1][qj - 1] <= s) {
+            cout << "TRUE\n";
+        } else {
+            cout << "FALSE\n";
+        }
+    }
+    return 0;
+}
