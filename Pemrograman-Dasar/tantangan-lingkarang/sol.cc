3b136e210832eef51fb299bb0304e40a71e65bad
Hasan Mubarok <hasanmubarok32@gmail.com>
2025-10-29 22:12:04 +0700

    implement tantangan lingkaran

--- Pemrograman-Dasar/tantangan-lingkarang/sol.cc
+++ Pemrograman-Dasar/tantangan-lingkarang/sol.cc
@@ -9,17 +9,27 @@ int main() {
   int x1, y1, r1, x2, y2, r2;
   cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
+  cout << "nilai x1: " << x1 << ", nilai y1: " << y1 << ", nilai r1: " << r1 << ", nilai x2: " << x2 << ", nilai y2: " << y2 << ", dan nilai r2: " << r2 << endl;
+
   int dx = x1 - x2;
   int dy = y1 - y2;
 
+  cout << "nilai dx: " << dx << ", dan nilai dy: " << dy << endl;
+
   int d2 = (dx * dx) + (dy * dy);
+  cout << "nilai d2: " << d2 << endl;
 
   int sum = r1 + r2;
+  cout << "nilai sum: " << sum << endl;
   int diff = abs(r1 - r2);
+  cout << "nilai diff: " << diff << endl;
   int sum2 = sum * sum;
+  cout << "nilai sum2: " << sum2 << endl;
   int diff2 = diff * diff;
+  cout << "nilai diff2: " << diff2 << endl;
 
   if (diff2 <= d2 && d2 <= sum2) {
+    cout << "masukk, nilai diff2: " << diff2 << ", nilai d2: " << d2 << ", dan nilai sum2: " << sum2 << endl;
     cout << "bersentuhan\n";
   } else {
     cout << "tidak bersentuhan\n";
