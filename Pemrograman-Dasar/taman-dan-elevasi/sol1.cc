5a93aa0c633fb742815c45a6f1683205f657164d
Hasan Mubarok <hasanmubarok32@gmail.com>
2026-09-14 11:59:47 +0700

    init taman dan elevasi

---
+++ Pemrograman-Dasar/taman-dan-elevasi/sol1.cc
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct DSU {
+  int n;
+  vector<int> p, sz;
+
+  DSU(int n = 0): n(n), p(n), sz(n, 1) {
+    iota(p.begin(), p.end(), 0);
+  }
+
+  int find(int x) {
+    return p[x] == x ? x : p[x] = find(p[x]);
+  }
+
+  bool unite(int a, int b) {
+    a = find(a);
+    b = find(b);
+
+    if (a == b) return false;
+    if (sz[a] < sz[b]) {
+      swap(a, b);
+    }
+    p[b] = a;
+    sz[a] += sz[b];
+    return true;
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> a(n * m);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> a[i * m + j];
+    }
+  }
+
+  // collect cells (height, index)
+  vector<pair<int, int>> cells;
+  cells.reserve(n * m);
+
+  for (int idx = 0; idx < n * m; idx++) {
+    cells.emplace_back(a[idx], idx);
+  }
+
+  // sort descending by height
+  sort(cells.begin(), cells.end(), [](auto &A, auto &B) {
+      if (A.first != B.first) return A.first > B.first;
+      return A.second < B.second;
+  });
+
+  DSU dsu(n * m);
+  vector<char> active(n * m, 0);
+  int currComponents = 0;
+  int ans = 0;
+
+  auto id_to_rc = [&](int id) {
+    return make_pair(id / m, id % m);
+  };
+
+  int i = 0;
+  while (i < (int)cells.size()) {
+    int val = cells[i].first;
+    // current state correspond to x = val (active = heights > val)
+    ans = max(ans, currComponents);
+
+    // add all cells with height = val
+    int j = i;
+    while (j < (int)cells.size() && cells[j].first == val) {
+      int idx = cells[j].second;
+      active[idx] = 1;
+      ++currComponents; // a new component initially
+      auto [r, c] = id_to_rc(idx);
+      // check 4 neighbors
+      const int dr[4] = {-1, 1, 0, 0};
+      const int dc[4] = {0, 0, -1, 1};
+      for (int d = 0; d < 4; d++) {
+        int nr = r + dr[d];
+        int nc = c + dc[d];
+        if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+        int nidx = nr * m + nc;
+        if (active[nidx]) {
+          // If they belong to different components, unite and decrease count
+          if (dsu.unite(idx, nidx)) {
+            --currComponents;
+          }
+        }
+      }
+      j++;
+    }
+    i = j;
+  }
+  // also consider x < min_height (after all cells added)
+  ans = max(ans, currComponents);
+  cout << ans << '\n';
+  return 0;
+}
