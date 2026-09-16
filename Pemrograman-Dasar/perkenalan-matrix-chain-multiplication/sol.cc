d2eb25b7780081eec1ce5899b19f7976a1293946
Hasan Mubarok <hasanmubarok32@gmail.com>
2026-09-16 13:18:57 +0700

    debug perkenalan matrix chain multiplication

--- Pemrograman-Dasar/perkenalan-matrix-chain-multiplication/sol.cc
+++ Pemrograman-Dasar/perkenalan-matrix-chain-multiplication/sol.cc
@@ -37,23 +37,39 @@ int main() {
 
   // len = number of matrices in the interval
   for (int len = 2; len <= N; len++) {
+    cout << "nilai len: " << len << endl;
     for (int i = 0; i + len - 1 < N; i++) {
+      cout << "nilai i: " << i << endl;
       int j = i + len - 1;
+      cout << "nilai j: " << j << endl;
 
       dp[i][j] = LLONG_MAX;
       ways[i][j] = 0;
       all[i][j] = 0;
 
       for (int k = i; k < j; k++) {
+        cout << "nilai k: " << k << endl;
+        cout << "nilai dp[i][k]: " << dp[i][k] << endl;
+        cout << "nilai dp[k + 1][j]: " << dp[k + 1][j] << endl;
+        cout << "nilai A[i]: " << A[i] << endl;
+        cout << "nilai A[k + 1]: " << A[k + 1] << endl;
+        cout << "nilai A[j + 1]: " << A[j + 1] << endl;
         // Cost of splitting
         // (M[i] ... M[k]) * (M[k + 1] ... M[j])
         ll cost = dp[i][k] + dp[k + 1][j] + A[i] * A[k + 1] * A[j + 1];
+        cout << "nilai cost: " << cost << endl;
 
         // Count ALL possible ways
+        cout << "nilai all[i][j]: " << all[i][j] << endl;
+        cout << "nilai all[i][k]: " << all[i][k] << endl;
+        cout << "nilai all[k + 1][j]: " << all[k + 1][j] << endl;
         all[i][j] = (all[i][j] + all[i][k] * all[k + 1][j]) % MOD;
+        cout << "nilai yang baru dari all[i][j]: " << all[i][j] << endl;
 
         // Count minimum-cost ways
         ll currentWays = (ways[i][k]* ways[i + 1][j]) % MOD;
+        cout << "nilai currentWays: " << currentWays << endl;
+        cout << "nilai cost: " << cost << ", dan nilai dp[i][j]: " << dp[i][j] << endl;
 
         if (cost < dp[i][j]) {
           dp[i][j] = cost;
@@ -63,7 +79,9 @@ int main() {
           ways[i][j] %= MOD;
         }
       }
+      cout << "nilai i: " << i << endl;
     }
+    cout << "akhir untuk len: " << len << endl << endl;
   }
 
   if (Q == 1) {
