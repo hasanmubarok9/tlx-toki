fa3c0ad0d0ec5b2cda91b39b9dc734a1f6f3f5e8
Hasan Mubarok <hasanmubarok32@gmail.com>
2026-09-18 12:38:47 +0700

    init penghancuran batu

---
+++ Pemrograman-Dasar/penghancuran-batu/sol.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int N;
+  int* arr;
+
+  unsigned long long sum = 0;
+
+  scanf("%d", &N);
+
+  arr = malloc(N * sizeof(int));
+
+  for (int i = 0; i < N; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  for (int i = 1; i < N; i++) {
+    int x = (arr[i - 1] > arr[i] ? arr[i - 1] : arr[i]);
+    sum += x;
+  }
+
+  printf("%llu\n", sum);
+
+  return 0;
+}
