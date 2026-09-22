4c2747c2dfc2ab3043117d0bfe121e695fa01944
Hasan Mubarok <hasanmubarok32@gmail.com>
2026-09-22 10:15:11 +0700

    debug kalkulator pengubah basis

--- Pemrograman-Dasar/kalkulator-pengubah-basis/sol.cc
+++ Pemrograman-Dasar/kalkulator-pengubah-basis/sol.cc
@@ -22,10 +22,13 @@ int main() {
   dp[0] = 1; // base case
 
   for (int i = 0; i < n; i++) {
+    cout << "nilai i: " << i << ", dan nilai dp[i]: " << dp[i] << endl;
     if (dp[i] == 0) continue; // if position i cannot be reached, skip it
 
     // case 1: substring started with '0'
     if (s[i] == '0') {
+      cout << "case 1, substring started with '0'\n";
+      cout << "nilai dp[i]: " << dp[i] << ", dan nilai dp[i + 1]: " << dp[i + 1] << endl;
       // '0' can only stand alone as the digit 0
       if (0 < b) {
         dp[i + 1] = (dp[i + 1] + dp[i]) % MOD;
@@ -36,7 +39,9 @@ int main() {
     else {
       long long val = 0;
       for (int j = i; j < n; j++) {
+        cout << "nilai j: " << j << ", dan nilai s[j]: " << s[j] << ", dan nilai val: " << val << endl;;
         val = val * 10 + (s[j] - '0');
+        cout << "nilai val baru: " << val << endl;
 
         // If the substring value exceeds or is equal to b, then
         // the longer truncation is definitely invalid (break)
@@ -48,7 +53,19 @@ int main() {
         dp[j + 1] = (dp[j + 1] + dp[i]) % MOD;
       }
     }
+
+    cout << "akhir untuk i: " << i << ", nilai dp: ";
+    for (int k = 0; k <= n; k++) {
+      cout << dp[k] << ' ';
+    }
+    cout << '\n' << '\n';
   }
+
+  cout << "nilai dp akhir: ";
+  for (int i = 0; i < n + 1; i++) {
+    cout << dp[i] << ' ';
+  }
+  cout << '\n';
 
   // The final result is dp[n]
   cout << dp[n] << '\n';
